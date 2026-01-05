b809ab83afce193a676bcc62556c4d97de0a29c8
Gabriel Thornburgh <gabriel.thornburgh1@gmail.com>
2026-01-05 13:58:41 -0500

    Local Storage

--- Events/script.js
+++ Events/script.js
@@ -6,57 +6,57 @@ document.addEventListener('DOMContentLoaded',function(){
     let newColor;
     let counter = 0;
 
-document.querySelector('form').addEventListener('submit',function(e){
-    e.preventDefault();
-        if(colorInput.value !== ''){
-            newColor = colorInput.value;
+    form.addEventListener('submit',function(e){
+        e.preventDefault();
+            if(colorInput.value !== ''){
+                newColor = colorInput.value;
+            }
+
+            Array.from(document.getElementsByClassName('box')).forEach(box => {
+                box.style.backgroundColor = `${newColor}`;        
+            });
+    });
+
+
+    const boxMaker = function(){
+        const newBox =document.createElement('div');
+        newBox.textContent = `box-${counter}`;
+        newBox.style.backgroundColor = newColor;
+        newBox.className = 'box';
+        newBox.id = counter;
+        counter++;
+
+        boxContainer.appendChild(newBox);
+    }
+
+    button.addEventListener('click',boxMaker);
+
+    document.addEventListener('dblclick',function(e){
+        if(e.target.classList.contains('box')){
+            e.target.remove();
         }
+    })
 
-        Array.from(document.getElementsByClassName('box')).forEach(box => {
-            box.style.backgroundColor = `${newColor}`;        
-        });
-});
+    document.addEventListener('mouseover',function(e){
+        if(e.target.classList.contains('box')){
+            e.target.textContent =`X: ${e.pageX} Y: ${e.pageY}`;
+        }
+    })
 
+    document.addEventListener('mouseout',function(e){
+        if(e.target.classList.contains('box')){
+            e.target.textContent =`box-${e.target.id}`;
+        }
+    })
 
-const boxMaker = function(){
-    const newBox =document.createElement('div');
-    newBox.textContent = `box-${counter}`;
-    newBox.style.backgroundColor = newColor;
-    newBox.className = 'box';
-    newBox.dataset.ID = counter;
-    counter++;
-
-    boxContainer.appendChild(newBox);
-}
-
-document.getElementById('new-box-button').addEventListener('click',boxMaker);
-
-document.addEventListener('dblclick',function(e){
-    if(e.target.classList.contains('box')){
-        e.target.remove();
-    }
-})
-
-document.addEventListener('mouseover',function(e){
-    if(e.target.classList.contains('box')){
-        e.target.textContent =`X: ${e.pageX} Y: ${e.pageY}`;
-    }
-})
-
-document.addEventListener('mouseout',function(e){
-    if(e.target.classList.contains('box')){
-        e.target.textContent =`box-${e.target.dataset.ID}`;
-    }
-})
-
-document.addEventListener('keydown',function(e){
-    if(e.target.id === 'color-input'){
-        return;
-    }
-    if(e.key.toLowerCase() === 'n'){
-        boxMaker();
-    }
-})
+    document.addEventListener('keydown',function(e){
+        if(e.target.id === 'color-input'){
+            return;
+        }
+        if(e.key.toLowerCase() === 'n'){
+            boxMaker();
+        }
+    })
 
 
 
